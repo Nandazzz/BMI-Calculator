48f77c49af43bd4be0638723387e91744b70d44f
Nandazzz <123712584+Nandazzz@users.noreply.github.com>
2023-04-08 14:38:00 +0700

    revisi jenis pembulatan nilai

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function calculateBMI() {
   let weight = document.getElementById("weight-kg").value;
   let height = document.getElementById("height-cm").value;
-  let bmi = weight / Math.pow(height / 100, 2);
+  let bmi = weight / (height * height);
   let result = document.getElementById("result");
   if (bmi < 18.5) {
     result.textContent = `Your BMI is ${
-      Math.ceil(bmi * 10) / 10
+      Math.round(bmi * 10) / 10
     } which means You are. Underweight`;
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     result.textContent = `Your BMI is ${
-      Math.ceil(bmi * 10) / 10
+      Math.round(bmi * 10) / 10
     } which means You are. Normal weight`;
   } else if (bmi >= 25 && bmi <= 29.9) {
     result.textContent = `Your BMI is ${
-      Math.ceil(bmi * 10) / 10
+      Math.round(bmi * 10) / 10
     } which means You are. Overweight`;
   } else {
     result.textContent = `Your BMI is ${
-      Math.ceil(bmi * 10) / 10
+      Math.round(bmi * 10) / 10
     } which means You are. Obesity`;
   }
 }
